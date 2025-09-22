07de131989d3e225b898c223746de0210b914a96
fede-vaccaro <federico.vaccaro@live.it>
2025-09-22 23:52:00 +0200

    first api implementation

---
+++ test/api.spec.cc
@@ -0,0 +1,211 @@
+#include <algorithm>
+#include <cstddef>
+#include <filesystem>
+#include <gtest/gtest.h>
+#include <random>
+
+#include <unordered_set>
+#include <uuid.h>
+#include <vector>
+
+#include "../api/config.h"
+#include "../api/api.h"
+
+#include "async/working_group.h"
+
+namespace hedge::db
+{
+    struct api_test : public ::testing::TestWithParam<std::tuple<size_t, size_t, size_t>>
+    {
+        void SetUp() override
+        {
+            this->N_KEYS = std::get<0>(GetParam());
+            this->PAYLOAD_SIZE = std::get<1>(GetParam());
+            this->MEMTABLE_CAPACITY = std::get<2>(GetParam());
+
+            if(std::filesystem::exists(this->_base_path))
+                std::filesystem::remove_all(this->_base_path);
+        }
+
+        uuids::uuid generate_uuid()
+        {
+            this->_uuids.emplace_back(this->gen());
+            return this->_uuids.back();
+        }
+
+        std::vector<uint8_t> make_random_vec(size_t size)
+        {
+            std::vector<uint8_t> vec(size);
+
+            static std::uniform_int_distribution<uint8_t> dist(0, 255);
+
+            std::ranges::generate(vec, [this]()
+                                  { return dist(this->_generator); });
+            return vec;
+        }
+
+        static std::vector<uint8_t> make_random_vec_seeded(size_t size, size_t seed)
+        {
+
+            static std::unordered_map<size_t, std::vector<uint8_t>> cache;
+            constexpr size_t MAX_CACHE_ITEMS_CAPACITY = 1024;
+
+            if(size_t hash = seed % MAX_CACHE_ITEMS_CAPACITY; cache.contains(hash))
+                return cache[hash];
+
+            std::vector<uint8_t> vec(size);
+            std::mt19937 generator{seed};
+            std::uniform_int_distribution<uint8_t> dist(0, 255);
+
+            std::ranges::generate(vec, [&dist, &generator]()
+                                  { return dist(generator); });
+            cache[seed] = vec;
+            return vec;
+        }
+
+        void TearDown() override
+        {
+        }
+
+        // test parameters
+        size_t N_KEYS{};                 // number of keys per run
+        size_t PAYLOAD_SIZE{};           // payload size in bytes
+        size_t MEMTABLE_CAPACITY{};      // memtable capacity
+        bool TEST_DELETION{true};        // whether to test deletion or not
+        double DELETE_PROBABILITY{0.02}; // if deletion test is enabled, how many keys of total should be removed
+
+        // runtime
+        std::filesystem::path _base_path = "/tmp/db";
+
+        // rng and seed stuff
+        std::vector<uuids::uuid> _uuids;
+        std::unordered_set<uuids::uuid> _deleted_keys;
+        size_t seed{107279581};
+        std::mt19937 _generator{seed};
+        std::uniform_int_distribution<int> dist{0, 15};
+        uuids::basic_uuid_random_generator<std::mt19937> gen{_generator};
+    };
+
+    TEST_P(api_test, api_comprehensive_test)
+    {
+        this->_uuids.reserve(this->N_KEYS);
+
+        async::working_group write_wg;
+        write_wg.set(this->N_KEYS);
+
+        config config;
+        config.auto_compaction = true;
+        config.keys_in_mem_before_flush = this->MEMTABLE_CAPACITY;
+        config.num_partition_exponent = 0; // single partition
+        config.compaction_read_ahead_size_bytes = 4 * 1024 * 1024;
+
+        auto maybe_db = api::create_database(this->_base_path, config);
+        ASSERT_TRUE(maybe_db) << "An error occurred while creating the database: " << maybe_db.error().to_string();
+
+        auto db = std::move(maybe_db.value());
+
+        std::uniform_real_distribution<double> dist(0.0, 1.0);
+
+        auto t0 = std::chrono::high_resolution_clock::now();
+        for(size_t i = 0; i < this->N_KEYS; ++i)
+        {
+            auto key = this->generate_uuid();
+            auto value = api_test::make_random_vec_seeded(this->PAYLOAD_SIZE, i);
+
+            db->put(key,
+                    std::move(value),
+                    [&write_wg](auto status)
+                    {
+                        if(!status)
+                            std::cerr << "An error occurred during insertion: " << status.error().to_string() << std::endl;
+                        write_wg.decr();
+                    });
+        }
+
+        write_wg.wait();
+        auto t1 = std::chrono::high_resolution_clock::now();
+
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0);
+        std::cout << "Total duration for insertion: " << (double)duration.count() / 1000.0 << " ms" << std::endl;
+        std::cout << "Average duration per insertion: " << (double)duration.count() / this->N_KEYS << " us" << std::endl;
+        std::cout << "Insertion throughput: " << (uint64_t)(this->N_KEYS / (double)duration.count() * 1'000'000) << " items/s" << std::endl;
+        std::cout << "Insertion bandwidth: " << (double)this->N_KEYS * (this->PAYLOAD_SIZE / 1024.0) / (duration.count() / 1000.0) << " MB/s" << std::endl;
+        std::cout << "Deleted keys: " << this->_deleted_keys.size() << std::endl;
+
+        // compaction
+        t0 = std::chrono::high_resolution_clock::now();
+        auto compaction_status_future = db->compact_sorted_indices(true);
+        ASSERT_TRUE(compaction_status_future.get()) << "An error occurred during compaction: " << compaction_status_future.get().error().to_string();
+        t1 = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0);
+        std::cout << "Total duration for compaction: " << (double)duration.count() / 1000.0 << " ms" << std::endl;
+
+        EXPECT_DOUBLE_EQ(db->load_factor(), 1.0) << "Read amplification should be 1.0 after compaction";
+
+        async::working_group read_wg;
+        read_wg.set(this->N_KEYS);
+
+        size_t number_of_errors = 0;
+
+        t0 = std::chrono::high_resolution_clock::now();
+        for(size_t i = 0; i < this->N_KEYS; ++i)
+        {
+            db->get(this->_uuids[i],
+                    [i, &read_wg, this, &number_of_errors](expected<std::vector<uint8_t>> maybe_value)
+                    {
+                        if(!maybe_value)
+                        {
+                            number_of_errors++;
+                            std::cerr << "An error occurred during retrieval for key " << this->_uuids[i] << ": " << maybe_value.error().to_string() << std::endl;
+                            read_wg.decr();
+                            return;
+                        }
+
+                        auto& value = maybe_value.value();
+
+                        auto expected_value = api_test::make_random_vec_seeded(this->PAYLOAD_SIZE, i);
+                        if(value != expected_value)
+                        {
+                            std::cerr << "Retrieved value does not match expected value for item nr.  " << i << std::endl;
+                            number_of_errors++;
+                        }
+
+                        read_wg.decr();
+                    });
+        }
+
+        read_wg.wait();
+        t1 = std::chrono::high_resolution_clock::now();
+
+        if(number_of_errors > 0)
+        {
+            std::cerr << "Number of errors: " << number_of_errors << std::endl;
+            FAIL() << "Some errors occurred during retrieval.";
+        }
+
+        duration = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0);
+        std::cout << "Total duration for retrieval: " << (double)duration.count() / 1000.0 << " ms" << std::endl;
+        std::cout << "Average duration per retrieval: " << (double)duration.count() / this->N_KEYS << " us" << std::endl;
+        std::cout << "Retrieval throughput: " << (uint64_t)(this->N_KEYS / (double)duration.count() * 1'000'000) << " items/s" << std::endl;
+        std::cout << "Retrieval bandwidth: " << (double)this->N_KEYS * (this->PAYLOAD_SIZE / 1024.0) / (duration.count() / 1000.0) << " MB/s" << std::endl;
+    }
+
+    INSTANTIATE_TEST_SUITE_P(
+        test_suite,
+        api_test,
+        testing::Combine(
+            testing::Values(30'000'000), // n keys
+            testing::Values(100),         // payload size
+            testing::Values(10'000'000)   // memtable capacity
+            ),
+        [](const testing::TestParamInfo<api_test::ParamType>& info)
+        {
+            auto num_keys = std::get<0>(info.param);
+            auto payload_size = std::get<1>(info.param);
+            auto memtable_capacity = std::get<2>(info.param);
+
+            std::string name = "N_" + std::to_string(num_keys) + "_P_" + std::to_string(payload_size) + "_C_" + std::to_string(memtable_capacity);
+            return name;
+        });
+
+} // namespace hedge::db
